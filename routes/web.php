72c86bf73867eb5841905a6940164d57f0fab81a
nava <nava.bogatee@gmail.com>
2021-01-20 00:25:57 +0700

    fixed responsive layout x-scroll for laptop 150% view

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,9 @@ Route::get('mail', function(){
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
+Route::get('account/register', function(){
+    return redirect('register');
+});
 Route::get('shareholder/{id?}',[ShareholderController::class, 'getShareholder']);
 Route::get('shareholder/delete/{id}', [ShareholderController::class, 'delete']);
 Route::get('shareholders',[ShareholderController::class, 'index']);
