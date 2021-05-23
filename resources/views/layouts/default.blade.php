de0e447f0c99ce51cfa2bf8465378e8cb5ad7666
nava <nava.bogatee@gmail.com>
2021-05-23 12:54:04 +0700

    updated google tag manager

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,17 +14,32 @@
     @yield('custom_css')
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FNJB1MNNTB"></script>
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-FNJB1MNNTB"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-FNJB1MNNTB');
-    </script>
+    </script> -->
+
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5QGKNX7');</script>
+    <!-- End Google Tag Manager -->
 
 </head>
 <body>
-    <div style="position:absolute;top:-999px">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QGKNX7"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <div style="display:none;visibility:hidden">
         {{ config('app.name', 'NEPSE.TODAY') }} is a portfolio management system for Nepal stock exchange (#NEPSE) and is available for FREE.
         Some of the major features are :
         <ul>
